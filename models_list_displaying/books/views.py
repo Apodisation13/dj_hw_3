e1079154ffb19d90a7b6a3a69bd7eb2a7e807e4e
Pavel Soloshenko <apodisation13@gmail.com>
2021-06-27 19:53:19 +0300

    books_start

---
+++ models_list_displaying/books/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+from .models import *
+from django.core.paginator import Paginator
+from datetime import datetime
+
+
+def books(request):
+    return redirect('books')
+
+
+def books_view(request):
+    template = 'books/books_list.html'
+    catalog = Book.objects.all()
+    context = {'books': catalog}
+    return render(request, template, context)
+
+
+def data_view(request, data):
+    template = 'books/book_date.html'
+    # sort_data = request.GET.get(data)
+    # print(sort_data)
+    catalog = Book.objects.filter(pub_date=data)
+
+
+    next_date = Book.objects.filter(pub_date__gt=data).\
+        values_list('pub_date').order_by('pub_date').first()[0].strftime("%Y-%m-%d")
+    print(type(next_date))
+    prev_date = Book.objects.filter(pub_date__lt=data).\
+        values_list('pub_date').order_by('-pub_date').first()[0].strftime("%Y-%m-%d")
+    print(prev_date)
+
+    context = {
+        'books': catalog,
+        'b_prev': prev_date,
+        'b_next': next_date
+    }
+
+    # page = int(request.GET.get('page', 1))
+    # elements_per_page = 10
+    # paginator = Paginator(catalog, elements_per_page)
+    # page_ = paginator.get_page(page)
+    # content = page_.object_list
+    #
+    # context = {
+    #     'bus_stations': content,
+    #     'page': page_,
+    # }
+    return render(request, template, context)
